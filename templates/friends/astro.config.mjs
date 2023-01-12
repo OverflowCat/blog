9ef5342ea3a1b46af3b52afc5457d5b5a2eaf06f
猫猫 <overflowcat@gmail.com>
2023-01-12 23:40:43 +0800

    chore: remove dev files

--- templates/friends/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    publicDir: "../../assets/",
-    outDir: "../../source/friends/"
-});
