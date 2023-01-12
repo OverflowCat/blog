38cd81f104e025806c2b988f6fb613c2f65b7be6
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2023-01-12 23:44:57 +0800

    feat: adding Astro (#2)
    * feat: add astro
    
    * page: about & friends
    
    * ci: add astro build
    
    * chore: remove dev files

---
+++ astro/astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// math
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// https://astro.build/config
+export default defineConfig({
+    outDir: "../public_astro/",
+    integrations: [svelte(), mdx()],
+    markdown: {
+        remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex],
+    }
+});
---
+++ build_fin.js
@@ -0,0 +1,25 @@
+// ES6 version using asynchronous iterators, compatible with node v10.0+
+
+const fs = require("fs");
+const path = require("path");
+
+async function* walk(dir) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
+
+// Then, use it with a simple async for loop
+async function main() {
+    for await (const p of walk('./public_astro')) {
+        console.log(p);
+        const new_path = p.replace(/^public_astro/, "public");
+        if (fs.existsSync(new_path))
+            fs.rmSync(new_path);
+        fs.renameSync(p, new_path);
+    }
+}
+
+main();
