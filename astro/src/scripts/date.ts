eb85348a45c06ee45ab5dc8c17f71b408a42daaa
猫猫 <overflowcat@gmail.com>
2023-07-10 06:58:09 +0800

    refactor: migrate to Astro

---
+++ astro/src/scripts/date.ts
@@ -0,0 +1,9 @@
+export function formatPostDate(date?: Date): string {
+  if (!date) {
+    return "No date";
+  }
+  console.log(date);
+  const offset = date.getTimezoneOffset();
+  date = new Date(date.getTime() - offset * 60 * 1000);
+  return date.toISOString().split("T")[0];
+}
