a4444e971f1e8660c1c2f8a486d30589afb213ce
猫猫 <overflowcat@gmail.com>
2023-07-12 03:09:57 +0800

    feat: year & og for `<ArticleList />`

--- src/scripts/open-graph.ts
+++ src/scripts/open-graph.ts
@@ -8,17 +8,19 @@ export interface OpenGraphData {
 
 export type PathToSupportedImage = `${string}.${"png" | "jpg" | "jpeg"}`;
 
-export function genOg(data: OpenGraphData) {
+export function genOpenGraphMetas(data: OpenGraphData) {
+  const pngLogo =
+    "https://user-images.githubusercontent.com/20166026/252747732-d43d721d-1aea-4762-bd9e-7e48f7186bce.png";
   return {
     "og:title": data.title,
     "og:description": data.description,
-    "og:image": data.image || "",
+    "og:image": data.image || pngLogo,
     "og:url": data.url,
     "og:type": data.type ?? "article",
     "twitter:card": "summary_large_image",
     "twitter:site": "@lazy_static",
     "twitter:creator": "@lazy_static",
     "twitter:description": data.description,
-    "twitter:image": data.image || "",
+    "twitter:image": data.image || pngLogo,
   };
 }
