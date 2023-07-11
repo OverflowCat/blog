5f5665d303105b58dbedf9a1bc9ac2f3b7bc9819
猫猫 <overflowcat@gmail.com>
2023-07-11 17:18:33 +0800

    chore: remove Hexo

--- src/scripts/date.ts
+++ src/scripts/date.ts
@@ -1,8 +1,5 @@
 export function formatPostDate(date?: Date): string {
-  if (!date) {
-    return "No date";
-  }
-  console.log(date);
+  if (!date) return "No date";
   const offset = date.getTimezoneOffset();
   date = new Date(date.getTime() - offset * 60 * 1000);
   return date.toISOString().split("T")[0];
