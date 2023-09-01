486370604eeb9f23e51608952a47df88b1c35486
OverflowCat <overflowcat@gmail.com>
2023-09-01 15:12:46 +0800

    style: `font-size` and `line-height`

--- src/scripts/date.ts
+++ src/scripts/date.ts
@@ -2,5 +2,6 @@ export function formatPostDate(date?: Date): string {
   if (!date) return "No date";
   const offset = date.getTimezoneOffset();
   date = new Date(date.getTime() - offset * 60 * 1000);
-  return date.toISOString().split("T")[0];
+  // mm-dd
+  return `${date.getMonth() + 1}-${date.getDate()}`;
 }
