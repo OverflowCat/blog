a6ea4911b5762b64d60da3bd89ba1d948f88b8a3
OverflowCat <overflowcat@gmail.com>
2024-02-25 17:30:21 +0800

    refactor: use Astro.locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+    title: string;
+    metas: any;
+    noscript: boolean;
+  }
+}
