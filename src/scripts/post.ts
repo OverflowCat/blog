b9d728e5e4162fcf5e9b0d159f7e614c724b4197
OverflowCat <overflowcat@gmail.com>
2024-07-02 05:43:21 +0800

    fix: atom feed

--- src/scripts/post.ts
+++ src/scripts/post.ts
@@ -1,6 +1,6 @@
 import { getCollection } from "astro:content";
 
-export async function getBlogPosts(): Promise<BlogPost[]> {
+export async function getBlogPosts() {
   const posts = await getCollection<"blog">("blog");
   return posts.filter((post) => post.data.draft !== true);
 }
