a255dd1afd37c13505830064ee58578712b30347
OverflowCat <overflowcat@gmail.com>
2024-08-20 00:03:17 +0800

    fix: build issue

--- astro.config.ts
+++ astro.config.ts
@@ -122,7 +122,7 @@ export default defineConfig({
 		},
 		ssr: {
 			external: ["prismjs", "@myriaddreamin/typst-ts-node-compiler"],
-			noExternal: ["modern-css-reset", "xp.css", "98.css"],
+			noExternal: ["modern-css-reset", "xp.css", "98.css", "/@astro-community/"],
 		},
 	},
 	markdown: {
