4829c902aff4d5aced39f9b52d413e7a9de7cc6a
OverflowCat <overflowcat@gmail.com>
2024-12-04 01:31:37 +0800

    post: better translation

--- src/scripts/date.ts
+++ src/scripts/date.ts
@@ -2,6 +2,7 @@ export function formatPostDate(date?: Date): string {
   if (!date) return "No date";
   const offset = date.getTimezoneOffset();
   date = new Date(date.getTime() - offset * 60 * 1000);
-  // mm-dd
-  return `${date.getMonth() + 1}-${date.getDate()}`;
+  const mm /* 2 digits */ = `0${date.getMonth() + 1}`.slice(-2);
+  const dd /* 2 digits */ = `0${date.getDate()}`.slice(-2);
+  return `${mm}-${dd}`;
 }
