78c68efd6d23dbbac0602e9c59fa9614d603e43b
OverflowCat <overflowcat@gmail.com>
2024-12-12 02:11:10 +0800

    ci: run after build

--- cli/static.js
+++ cli/static.js
@@ -3,7 +3,7 @@ import fs from "fs";
 const entries = [
     // ["./src/posts/weekly/0x07/tart.jpg", "./dist/tart.jpg"],
     // ["./dist/_astro/paw-licking.v22o3NZL_27GX7V.png", "./dist/_astro/paw-licking.v22o3NZL_RdBpb.png"]
-    ["node_modules/@moonbit/moonpad/dist/lsp-server.js", "public/lsp-server.js"],
+    ["node_modules/@moonbit/moonpad/dist/lsp-server.js", "./dist/lsp-server.js"],
 ]
 
 for (const [src, dest] of entries) {
