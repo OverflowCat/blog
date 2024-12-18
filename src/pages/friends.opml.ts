6fd7007ae7a52d5e90c31d32fbff28041d0a14a7
OverflowCat <overflowcat@gmail.com>
2024-12-18 19:07:51 +0800

    link: fix opml

--- src/pages/friends.opml.ts
+++ src/pages/friends.opml.ts
@@ -1,8 +1,20 @@
 import { generateFeedOpml } from "@/scripts/opml";
 import type { APIRoute } from "astro";
 
+const encoder = new TextEncoder();
+
 export async function GET(_: APIRoute) {
-    return new Response(
-      generateFeedOpml(),
-    )
-  }
+  const opml = generateFeedOpml();
+  const encodedOpml = encoder.encode(opml);
+  return new Response(encodedOpml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8"
+    }
+  });
+  // return {
+  //   body: opml,
+  //   headers: {
+  //     "Content-Type": "application/xml; charset=utf-8"
+  //   },
+  // }
+}
--- src/scripts/opml.ts
+++ src/scripts/opml.ts
@@ -7,20 +7,32 @@ interface Feed {
 
 let feeds = new Map<string, Feed>();
 
+function escapeXml(unsafe: string) {
+    return unsafe.replace(/[<>&'"]/g, (c) => {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+            default: return c;
+        }
+    });
+}
+
 export function generateFeedOpml() {
     const feedList: string[] = [];
     feeds.values()
         .forEach(
             (feed) => feedList.push(`
 <outline
-    text="${feed.title}"
-    title="${feed.title}"
+    text="${escapeXml(feed.title)}"
+    title="${escapeXml(feed.title)}"
     type="rss"
     xmlUrl="${feed.url}"
     htmlUrl="${feed.href}"
-    description="${feed.desc}"
-/>
-            `
+    description="${escapeXml(feed.desc)}"
+/>`
             ));
     if (feedList.length === 0) {
         throw new Error("No feeds collected");
