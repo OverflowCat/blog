fe3e897539365e7c254305ac565a60027b8d9407
OverflowCat <overflowcat@gmail.com>
2025-02-01 21:54:27 +0800

    fix: og & comment

--- src/scripts/open-graph.ts
+++ src/scripts/open-graph.ts
@@ -8,13 +8,18 @@ export interface OpenGraphData {
   type?: string;
 }
 
-export function genOpenGraphMetas(data: OpenGraphData) {
+export function genOpenGraphMetas(data: OpenGraphData, site: URL) {
   const pngLogo =
     "https://user-images.githubusercontent.com/20166026/252747732-d43d721d-1aea-4762-bd9e-7e48f7186bce.png";
+  let ogImage = pngLogo;
+  if (data.image) {
+    ogImage = data.image;
+    if (ogImage.startsWith("/")) ogImage = site + data.image;
+  }
   return {
     "og:title": data.title,
     "og:description": data.description,
-    "og:image": data.image || pngLogo,
+    "og:image": ogImage,
     "og:url": data.url,
     "og:type": data.type ?? "article",
     "twitter:card": "summary_large_image",
