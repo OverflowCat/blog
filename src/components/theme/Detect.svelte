0fdc2288636caaa3f8ce2b35afe1f99abc36e05c
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2025-03-10 15:16:28 +0800

    style: dark theme for `<Transcriptioin>`

---
+++ src/components/theme/Detect.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+export let theme: "light" | "dark";
+
+import { onMount } from "svelte";
+
+onMount(() => {
+	const select = document.getElementById("theme-select") as HTMLSelectElement;
+	if (!select) console.error("Theme select not found");
+	// add event listener to select
+	select.addEventListener("change", (e) => {
+		let v = select.value as "light" | "dark" | "system";
+		console.log("Theme changed to", v);
+		if (v === "system") {
+			// get system theme
+			v = window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark"
+				: "light";
+		}
+		theme = v;
+	});
+	// document.querySelectorAll("[data-telegram-post]").forEach((elem) => {
+	// 	elem.setAttribute("data-dark", theme === "dark" ? "1" : "0");
+	// });
+});
+</script>
--- src/components/transcription/Index.svelte
+++ src/components/transcription/Index.svelte
@@ -1,50 +1,55 @@
 <script lang="ts">
-  import Word from "./Word.svelte";
-  import StaticWord from "./StaticWord.svelte";
-  type BeginTime = number;
-  type EndTime = number;
-  type Text = string;
-  type Speech = [BeginTime, EndTime, Text][];
-  export let data: {
-    s: Speech;
-    u: string;
-  }[];
-  export let speakers: Record<string, [string, string]>;
-  export let translations: string[];
-  if (translations) {
-    if (translations.length !== data.length) {
-      console.warn("Translations must match the number of parts");
-    }
-  }
-  let audioElem: HTMLAudioElement;
-  let currentTime = 0;
-  let nowPlaying: HTMLSpanElement;
+import Word from "./Word.svelte";
+import StaticWord from "./StaticWord.svelte";
+import Detect from "@/components/theme/Detect.svelte";
 
-  function changeTime(
-    beginTime: number,
-    endTime: number,
-    offset: number,
-    total: number
-  ) {
-    const duration = endTime - beginTime;
-    const progress = (offset / total) * duration;
-    currentTime = (beginTime + progress) / 1000;
-  }
+type BeginTime = number;
+type EndTime = number;
+type Text = string;
+type Speech = [BeginTime, EndTime, Text][];
+export let data: {
+	s: Speech;
+	u: string;
+}[];
+export let speakers: Record<string, [string, string, string]>;
+export let translations: string[];
+if (translations) {
+	if (translations.length !== data.length) {
+		console.warn("Translations must match the number of parts");
+	}
+}
+let audioElem: HTMLAudioElement;
+let currentTime = 0;
+let nowPlaying: HTMLSpanElement;
+// biome-ignore lint/style/useConst: binded to another component
+let theme: "light" | "dark" = "light";
 
-  function playing(elem: HTMLSpanElement) {
-    nowPlaying = elem;
-  }
+function changeTime(
+	beginTime: number,
+	endTime: number,
+	offset: number,
+	total: number,
+) {
+	const duration = endTime - beginTime;
+	const progress = (offset / total) * duration;
+	currentTime = (beginTime + progress) / 1000;
+}
 
-  function scrollTo() {
-    nowPlaying?.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
-  }
+function playing(elem: HTMLSpanElement) {
+	nowPlaying = elem;
+}
+
+function scrollTo() {
+	nowPlaying?.scrollIntoView({
+		behavior: "smooth",
+		block: "center",
+		inline: "center",
+	});
+}
 </script>
 
 <div class="transcription relative">
+  <Detect bind:theme />
   <div class="fixed inset-be-0 inset-is-0 inset-ie-0 shadow-md p-1 z-10">
     <audio
       class="inline-full drop-shadow-md"
@@ -67,10 +72,10 @@
 
   {#each data as part, i}
     <section class="speech">
-      <h4 class={speakers[part.u][1]}>
+      <h4 class={speakers[part.u][theme === "dark" ? 2 : 1]}>
         {speakers[part.u][0] || part.u}
       </h4>
-      <p lang="en" class={part.s[0][0] > currentTime * 1000 ? "o" : ""}>
+      <p lang="en" class={part.s[0][0] > currentTime * 1000 ? "oa" : ""}>
         {#if part.s.at(-1)![1] < currentTime * 1000 || part.s[0][0] > currentTime * 1000}
           {#each part.s as word}
             <StaticWord {word} click={changeTime} />
@@ -94,44 +99,73 @@
 </div>
 
 <style lang="less">
-      .tex sub,
-    .latex sub,
-    .latex sup {
-      text-transform: uppercase;
-    }
+  .tex sub,
+  .latex sub,
+  .latex sup {
+    text-transform: uppercase;
+  }
 
-    .tex sub,
-    .latex sub {
-      vertical-align: -0.5ex;
-      margin-left: -0.1667em;
-      margin-right: -0.125em;
-    }
+  .tex sub,
+  .latex sub {
+    vertical-align: -0.5ex;
+    margin-left: -0.1667em;
+    margin-right: -0.125em;
+  }
 
-    .tex,
-    .latex,
-    .tex sub,
-    .latex sub {
-      font-size: 1em;
-    }
+  .tex,
+  .latex,
+  .tex sub,
+  .latex sub {
+    font-size: 1em;
+  }
 
-    .latex sup {
-      font-size: 0.85em;
-      vertical-align: 0.15em;
-      margin-left: -0.36em;
-      margin-right: -0.15em;
-    }
+  .latex sup {
+    font-size: 0.85em;
+    vertical-align: 0.15em;
+    margin-left: -0.36em;
+    margin-right: -0.15em;
+  }
   :global {
     .speech {
-      margin-block: .5em;
+      margin-block: 0.5em;
+      font-size: 1.15em;
+      h4 {
+        padding-inline: 0.5ch;
+      }
+      p {
+        line-height: 1.5em;
+        font-size: 1.1em;
+        margin-block: 0.2em;
+        transition: opacity 0.1s;
+        span:hover {
+          background-color: rgba(141, 208, 215, 0.345);
+          & ~ span {
+            opacity: 50%;
+          }
+        }
+        &:has(span:hover) {
+          opacity: 100%;
+        }
+      }
       p span {
         cursor: pointer;
-        transition: color 0.15s;
+        transition:
+          color 0.1s,
+          opacity 0.1s;
         &:hover {
           background-color: #7b7b7b58;
         }
       }
+      blockquote {
+        margin-block-start: 0.8em;
+        margin-block-end: 1.2em;
+      }
+
       .o {
         opacity: 55%;
+      }
+      .oa {
+        opacity: 78%;
       }
     }
   }
