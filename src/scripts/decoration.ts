2692867713b7381d66aecf9730b09f7f16433ab8
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2025-03-25 23:54:36 +0800

    style: harmony decoration

--- src/scripts/decoration.ts
+++ src/scripts/decoration.ts
@@ -4,6 +4,7 @@ export const decoration = z.union([
     z.literal("binary"),
     z.literal("world"),
     z.literal("langs"),
+    z.literal("harmony"),
 ]);
 
 export type Decoration = typeof decoration._type;
