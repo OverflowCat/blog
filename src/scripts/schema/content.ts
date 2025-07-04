79b4be462c04d733ffe9ea82f8a79dbd577e257f
OverflowCat <overflowcat@gmail.com>
2025-07-05 06:09:21 +0800

    style: transition:animate="initial" post: better translation for `gyeeom` feat: multi licence fix: add back `.license` in feed style: `text-autospace: no-autospace` for ko style: new icon style: reset italic for `<cite>` chore: bump

--- src/scripts/schema/content.ts
+++ src/scripts/schema/content.ts
@@ -18,89 +18,112 @@ const TAG_TYPE = z
 	.nullish()
 	.transform(transform2arr);
 
-const SERIES_TYPE = z.union([
-	z.literal("weekly"),
-	z.literal("harmony"),
-	z.literal("六不答對"),
-]).nullish();
+const SERIES_TYPE = z
+	.union([z.literal("weekly"), z.literal("harmony"), z.literal("六不答對")])
+	.nullish();
 
-export const postSchemaGen = (ctx: SchemaContext) => z
-	.object({
-		title: z.string(),
-		categories: TAG_TYPE,
-		tags: TAG_TYPE,
-		series: SERIES_TYPE,
-		photo: photo(ctx.image).optional(),
-		date: z.coerce.date(),
-		draft: z
-			.boolean()
-			.optional()
-			.transform((x) => (x === undefined ? false : x)), // TODO: remove this
-		visibility: z
-			.union([z.literal("public"), z.literal("unlisted")])
-			.optional(),
-		desc: z.string().optional(),
-		icon: z
-			.string()
-			.refine(
-				(x: string) => !x.includes(":") || ICON_PACKS_SET.has(x.split(":")[0]),
-			)
-			.optional(),
-		decoration: decoration.optional(),
-		lang: z.union([
-			// 汉语
-			z.literal("cmn"),
-			// 淮语
-			z.literal("juai"),
-			// 满语
-			z.literal("mnc"),
-			// 英语
-			z.literal("en"),
-		]).optional(),
-		math: z.optional(
-			z.union([z.literal("mathjax"), z.literal("katex"), z.literal("typst")]),
-		),
-		hide_title: z
-			.boolean()
-			.optional()
-			.transform((x) => (x === undefined ? false : x)),
-		noscript: z.boolean(),
-		licence: z.union([
-			z.literal("CcBy"),
-			z.literal("CcBySa"),
-			z.literal("CcByNc"),
-			z.literal("CcByNd"),
-			z.literal("CcByNcSa"),
-			z.literal("CcByNcNd"),
-			z.literal("Minzoku"),
-			z.literal("PD"),
-			z.string().nullable(),
-			z.literal(false),
-			z.undefined(),
-		]),
-		paper: z.boolean().default(true),
-		ext: z.union([
-			z.literal("md"), z.literal("mdx"), z.literal("typ")
-		]).nullish(),
-		resizable: z.boolean().default(true),
-		hant: z.boolean().optional(),
-		vert: z.boolean().optional(),
-		theme: z.boolean().or(z.string()).optional(),
-		base: z.string().url().optional(), // base URL for URL-only img
-	})
-	// .refine((data) => !(data.math && data.vert), {
-	// 	message: "Math is not supported in vertical mode.",
-	// })
-	// .refine((data) => !(data.math === "typst" && data.hant), {
-	// 	message: "OpenCC is not supported in Typst.",
-	// })
-	.transform((data) => {
-		if (data.math) {
-			if (data.hant !== true) data.hant = false;
-			if (data.math === "typst")
-				if (data.vert !== true) data.vert = false;
-		}
-		if (data.vert === undefined) data.vert = false;
-		if (data.hant === undefined) data.hant = false;
-		return data;
-	})
+const LICENCE_TYPE = z.union([
+	z.literal("CcBy"),
+	z.literal("CcBySa"),
+	z.literal("CcBySa4.0"),
+	z.literal("CcByNc"),
+	z.literal("CcByNd"),
+	z.literal("CcByNcSa"),
+	z.literal("CcByNcNd"),
+	z.literal("Minzoku"),
+	z.literal("PD"),
+	z.literal("KOGL"),
+]);
+
+const LICENCE_PART = z.object({
+	type: z.union([
+		z.literal("own"),
+		z.literal("use"),
+		z.literal("translate from"),
+	]),
+	licence: LICENCE_TYPE,
+	source: z.string().url().optional(),
+	author: z.string().optional(),
+	usage: z.string().optional(),
+});
+
+export type LicenceType = z.infer<typeof LICENCE_TYPE>;
+export type LicencePartType = z.infer<typeof LICENCE_PART>;
+
+export const postSchemaGen = (ctx: SchemaContext) =>
+	z
+		.object({
+			title: z.string(),
+			categories: TAG_TYPE,
+			tags: TAG_TYPE,
+			series: SERIES_TYPE,
+			photo: photo(ctx.image).optional(),
+			date: z.coerce.date(),
+			draft: z
+				.boolean()
+				.optional()
+				.transform((x) => (x === undefined ? false : x)), // TODO: remove this
+			visibility: z
+				.union([z.literal("public"), z.literal("unlisted")])
+				.optional(),
+			desc: z.string().optional(),
+			icon: z
+				.string()
+				.refine(
+					(x: string) =>
+						!x.includes(":") || ICON_PACKS_SET.has(x.split(":")[0]),
+				)
+				.optional(),
+			decoration: decoration.optional(),
+			lang: z
+				.union([
+					// 汉语
+					z.literal("cmn"),
+					// 淮语
+					z.literal("juai"),
+					// 满语
+					z.literal("mnc"),
+					// 英语
+					z.literal("en"),
+				])
+				.optional(),
+			math: z.optional(
+				z.union([z.literal("mathjax"), z.literal("katex"), z.literal("typst")]),
+			),
+			hide_title: z
+				.boolean()
+				.optional()
+				.transform((x) => (x === undefined ? false : x)),
+			noscript: z.boolean(),
+			licence: z.union([
+				LICENCE_TYPE,
+				z.array(LICENCE_PART),
+				z.string().nullable(),
+				z.literal(false),
+				z.undefined(),
+			]),
+			paper: z.boolean().default(true),
+			ext: z
+				.union([z.literal("md"), z.literal("mdx"), z.literal("typ")])
+				.nullish(),
+			resizable: z.boolean().default(true),
+			hant: z.boolean().optional(),
+			vert: z.boolean().optional(),
+			theme: z.boolean().or(z.string()).optional(),
+			base: z.string().url().optional(), // base URL for URL-only img
+		})
+		// .refine((data) => !(data.math && data.vert), {
+		// 	message: "Math is not supported in vertical mode.",
+		// })
+		// .refine((data) => !(data.math === "typst" && data.hant), {
+		// 	message: "OpenCC is not supported in Typst.",
+		// })
+		.transform((data) => {
+			if (data.math) {
+				if (data.hant !== true) data.hant = false;
+				if (data.math === "typst") if (data.vert !== true) data.vert = false;
+			}
+			if (data.vert === undefined) data.vert = false;
+			if (data.hant === undefined) data.hant = false;
+			return data;
+		});
