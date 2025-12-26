85d0c2332283755d5ac35d75ea57667029e8daea
OverflowCat <overflowcat@gmail.com>
2025-12-26 15:18:24 +0800

    fix: bad opml (resolve #1215)

--- src/pages/friends.opml.ts
+++ src/pages/friends.opml.ts
@@ -3,7 +3,7 @@ import type { APIRoute } from "astro";
 
 
 export async function GET(_: APIRoute) {
-  const opml = generateFeedOpml();
+  const opml = await generateFeedOpml();
   return new Response(opml, {
     headers: {
       "Content-Type": "application/xml; charset=utf-8"
